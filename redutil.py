a8a1baa06cfd1d930f44797db71ab9141fb7b86c
Redd <[Private]>
2017-12-20 18:54:04 -0500

    add and remove admin commands added

--- redutil.py
+++ redutil.py
@@ -6,8 +6,28 @@ def isprivchan(message):
         return True
     else:
         return False
+#check if user is allowed to use bot
+def canUse(message,db):
+    if isprivchan(message) is True:
+        return True
+    else:
+        serveradminroles = db.table(message.server.id + "_roleperms")
+        if message.server.owner == message.author:
+            return True
+        for role in message.server.Roles:
+            serveradmins = serveradminroles.get(queryer.rid == role.id)
+            if serveradmins is not None:
+                for role2 in message.author.Roles:
+                    if role.id == role2.id:
+                        if serveradmins["admin"] == "true":
+                            return True
+                        elif serveradmins["disable_botuse"] == "true":
+                            return False
+            else:
+                serveradminroles.insert({"rid": role.id,"admin": "false","disable_botuse": "false"})
+        return True
 #check if user is bot admin
-def isAdmin(message):
+def isAdmin(message,db):
     if isprivchan(message) is True:
         return False
     else:
